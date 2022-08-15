557e6e9b6d7c26055507c7237a42fe60a548ca97
Palash <palash.kanti.kundu.90@gmail.com>
2022-08-15 22:49:12 +0530

    Delete unnecessary files

--- learning/01-04.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-puts("First line");
-puts("Second Line");
-return 0;
-}
-
--- learning/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    char s[15];
-    scanf("%s", s);
-    puts("Hello");
-    return 0;
-}
